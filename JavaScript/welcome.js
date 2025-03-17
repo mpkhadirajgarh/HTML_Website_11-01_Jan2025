b76764f182740988d33bc5bc1dc71ed3bd34e8ae
mpkhadirajgarh <mpkhadirajgarh@gmail.com>
2025-03-17 12:15:19 +0530

    done

---
+++ JavaScript/welcome.js
@@ -0,0 +1,20 @@
+
+console.log("Wel Come to Java Script");
+
+// variable Var , Let , const kya Differece hai 
+
+
+let a = 50 ;
+let b = 80 ;
+
+a = 80;
+
+let Language = "java Script" ;
+
+let c = a + b ;
+
+console.log(c);
+
+
+
+console.log(Language);
