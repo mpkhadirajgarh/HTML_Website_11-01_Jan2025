cdd84925db534b273a2acb56723380d02f05a81c
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-18 12:23:29 +0530

    done

---
+++ JavaScript/Datatype/string.js
@@ -0,0 +1,31 @@
+let StudentName = "Kumkum"; // string
+
+let DcaStudent = `Rajkumar malviya`; // String
+
+
+
+// console.log(`Studentname ka data type :`, typeof StudentName);
+
+
+// console.log(typeof DcaStudent)
+
+let a = 10 ; // Number 
+
+let b = "10"; // String  double quots 
+
+console.log(a, 'Data type :', typeof a);
+
+console.log(b, 'Data type :', typeof b);
+
+console.log (a+b);
+
+// const 
+
+const d = 60 ;
+const e = 50 ; 
+
+// d = 80 ; Value not decleare again.
+
+console.log(d + e);
+
+
