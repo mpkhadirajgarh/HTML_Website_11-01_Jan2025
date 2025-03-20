2a8dddbc70ae076d2a2c872500336565f1ce76d2
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-20 12:19:17 +0530

    done

---
+++ JavaScript/Datatype/number.js
@@ -0,0 +1,49 @@
+// number data type 
+
+let a = 100 ; // number 
+
+let b = "50" ; // string 
+let A = 110 ; 
+
+console.log(typeof a);
+console.log(typeof b);
+
+const c = parseInt(b); // string to number conversasion.
+
+console.log(typeof c);
+
+let d = Number(b);
+
+console.log(typeof d);
+
+// sum opration 
+
+let sum = a + c + d ; // 50+60+60=170
+
+console.log(sum);
+
+// Subtraction 
+
+console.log(`Subtraction of a-d: `, (a-d)); // 100-60=40
+
+// Multiplication 
+
+console.log(`Multiplication of a x d : `, (a*d));
+
+// Division / Divide
+
+console.log(`Division of a / d : `, (a/d));
+
+
+// 400/600
+
+// console.log((400*100)/600,`%`);
+
+// modulus / Remainder  
+
+console.log(`Remainder of :`,(30%4));
+
+
+
+
+
