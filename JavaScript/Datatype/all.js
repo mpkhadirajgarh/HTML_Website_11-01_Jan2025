350a719e22b655120420fd0e4a86b9f2a9312c2a
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-21 12:26:06 +0530

    done

---
+++ JavaScript/Datatype/all.js
@@ -0,0 +1,38 @@
+// NNSSBBU
+
+// number 
+
+// null // type Object 
+
+let a = null ; 
+
+console.log(typeof a);
+
+// String 
+
+// Symbol 
+
+let b = Symbol("Aditya");
+
+console.log(typeof b);
+
+// BigInt 
+
+let c = BigInt(5124);
+
+console.log(typeof c);
+
+// Boolean // true // False 
+
+let d = false ; 
+
+console.log(typeof d);
+
+// undefined
+
+let e;
+
+console.log(typeof e);
+
+
+
