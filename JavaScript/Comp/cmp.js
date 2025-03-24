99825957cdd91a0bcde609014635edb4bc600e4d
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-24 11:59:52 +0530

    done

---
+++ JavaScript/Comp/cmp.js
@@ -0,0 +1,15 @@
+let age = 17 ; 
+
+// Vote ke liye eligible hai ya nhi hai ? 
+
+
+if(age>=18) {
+
+    console.log(`You are eligible For Voting, Because your age is: ${age}`);
+
+}
+
+else {
+
+    console.log(`You are not eligible For Voting, Because your age is: ${age}`)
+}
