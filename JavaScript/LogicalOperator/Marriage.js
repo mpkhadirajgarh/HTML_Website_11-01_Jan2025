d4a9ea3aefc261963b967a6aae06293a1083059e
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-25 12:14:13 +0530

    done

---
+++ JavaScript/LogicalOperator/Marriage.js
@@ -0,0 +1,20 @@
+let age =  20 ; 
+
+let Gender = "Male";
+
+if( Gender == "Male" && age>=21) {
+
+console.log(`You are Eligible for marriage Because your age is:${age} and Gender:${Gender}`)
+
+}
+
+else if(Gender == "Female" && age>=18) {
+
+    console.log(`You are Eligible for marriage Because your age is:${age} and Gender:${Gender}`)
+
+}
+
+else {
+
+    console.log(`You are NOT Eligible for Marriage Because you are Not fit Age Criteria: ${age} && Gender: ${Gender}`)
+}
