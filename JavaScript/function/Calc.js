451308ddca2819c01170ad4f3844a3964f2b4607
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-03-27 12:31:17 +0530

    done

---
+++ JavaScript/function/Calc.js
@@ -0,0 +1,19 @@
+function calc(value1, value2) {
+
+    console.log(`Value1 and Value2 Sum:`,value1+value2);
+    console.log(`Value1 and Value2 Sub:`,value1-value2);
+    console.log(`Value1 and Value2 Multiply:`,value1*value2);
+    console.log(`Value1 and Value2 Divide:`,value1/value2);
+    console.log(`Value1 and Value2 Remainder:`,value1 % value2);
+    
+}
+
+calc(11,2); // argument pass krna 
+
+function myfun(a,b) {
+
+    return a*b;
+
+}
+
+console.log(myfun(20,30));
---
+++ JavaScript/function/fun.js
@@ -0,0 +1,16 @@
+// Function with parameter  > value1 , value2 
+
+function AddTwoNumber(value1, value2){
+
+    console.log(`Sum of:`,value1+ value2)
+}
+
+AddTwoNumber(500,5001); // do argument pass kiye
+
+function abc(name){
+
+    console.log(`Your Name is:`,name);
+
+}
+
+abc("Rajkumar");
